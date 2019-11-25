8a8fedf091c7d11465609f5ed8ab6f816016b226
mplichta <bigcatplichta@gmail.com>
2019-11-25 16:02:12 -0700

    added some validations

--- app/models/keeper.rb
+++ app/models/keeper.rb
@@ -2,6 +2,9 @@ class Keeper < ApplicationRecord
     has_many :animals
     has_many :wishes, through: :animals
     has_many :toys, through: :wishes
-
     accepts_nested_attributes_for :animals
+
+    validates :password, confirmation: true
+    validates :password_confirmation, presence: true
+    
 end
