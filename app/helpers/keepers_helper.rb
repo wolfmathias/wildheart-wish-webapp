85f86845f8face1aee0270421026406b1da2f769
mplichta <bigcatplichta@gmail.com>
2019-11-26 21:31:54 -0700

    moved keeper_params to helper

--- app/helpers/keepers_helper.rb
+++ app/helpers/keepers_helper.rb
@@ -1,2 +1,9 @@
 module KeepersHelper
+
+    private
+
+    def keeper_params
+        params.require(:keeper).permit(:first_name, :last_name, :email, :password, :password_confirmation, animals_attributes: [:name, :species, :bio, toy_ids: []])
+    end
+    
 end
