d4232cebc6dba43c0f9633fb8d35adf611d6c890
mplichta <bigcatplichta@gmail.com>
2019-11-27 00:23:55 -0700

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,22 @@
 Rails.application.routes.draw do
   
-  resources :keepers, except: [:index]
-  resources :donors, except: [:index]
-
   resources :keepers, only: [:show] do
     resources :animals 
   end
-
+  
+  resources :keepers, except: [:index, :show]
+  
   resources :donors, only: [:show] do
     resources :donations, only: [:index, :show] 
     resources :animals, only: [:index, :show]
   end
 
-  get '/login', to: 'sessions#new'
-  post '/sessions', to: 'sessions#create'
+  resources :donors, except: [:index, :show]
 
+  
+
+  get '/login', to: 'sessions#new', as: 'new_session'
+  post '/sessions', to: 'sessions#create'
+  # post '/delete', to: 'sessions#destroy', as: '/'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
