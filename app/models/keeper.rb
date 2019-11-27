acf636a3f9475cb2d9984a8f15050fe894fe5a34
mplichta <bigcatplichta@gmail.com>
2019-11-26 21:32:24 -0700

    updated validations in keeper model

--- app/models/keeper.rb
+++ app/models/keeper.rb
@@ -4,7 +4,9 @@ class Keeper < ApplicationRecord
     has_many :toys, through: :wishes
     accepts_nested_attributes_for :animals
 
-    validates :password, confirmation: true
+    validates :password, confirmation: true, message: "Passwords must match."
     validates :password_confirmation, presence: true
+    validates :email, presence: true, message: "Please enter a valid email."
+    validates :email, uniqueness: true, message: "An account already exists with that email."
     
 end
