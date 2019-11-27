5b255cce352f43f3263b89103fead108cd30cb5a
mplichta <bigcatplichta@gmail.com>
2019-11-27 12:42:27 -0700

    added roles to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ApplicationRecord
+    # model being built to encompass all user instead of separate models
     has_secure_password
+    enum role: [:donor, :keeper, :admin] # set role by calling self.donor!, self.keeper!, self.admin!
+
+    # set default role to donor
+    after_initialize do
+        if self.new_record?
+            self.role ||= :donor 
+        end
+    end
+
+
+    
 
 end
