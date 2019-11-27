bdd604f2b942357e636ca7eddaa9ec3aaa6f431e
mplichta <bigcatplichta@gmail.com>
2019-11-26 23:06:25 -0700

    added some methods to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+    helper_method :current_user
 
     def new
     end
@@ -8,16 +9,21 @@ class SessionsController < ApplicationController
         # send to show page if no errors
         # redirect if errors
     end
+
+    def destroy
+        session.delete(:user_id)
+    end
     
     def current_user
-        
+        @current_user ||= User.find_by(id: session[:user_id])
     end
 
     def logged_in?
-
+        !!current_user
     end
 
     def set_user
+        # where to check for user type? Separate controllers for different user types?
         @user = User.find_by(user_params)
     end
 
