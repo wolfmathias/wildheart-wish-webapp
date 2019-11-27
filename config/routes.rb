8bb5f562b5d623d9abfe26173539b81919608ebb
mplichta <bigcatplichta@gmail.com>
2019-11-26 21:35:08 -0700

    added routes for session new and create actions

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,8 @@ Rails.application.routes.draw do
     resources :animals, only: [:index, :show]
   end
 
-
+  get '/login', to: 'sessions#new'
+  post '/sessions', to: 'sessions#create'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
