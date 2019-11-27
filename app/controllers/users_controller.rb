cbf39105ba997f97e25bb373570a84d17229dd57
mplichta <bigcatplichta@gmail.com>
2019-11-27 00:31:46 -0700

    create methods w/ strong params and authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+
+    def new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save?
+            redirect_to user_path(@user)
+        else
+            render 'users/new'
+        end
+    end
+
+    private
+
+    def user_params 
+        params.require(:user).permit( # ensure all keys from signup form are here
+            :email,
+            :password
+        )
+    end
+
 end
